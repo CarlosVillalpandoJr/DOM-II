33b35e08effaa01dd54ed86ff6d52f9cd63e3526
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-07-14 19:33:07 -0500

    added event listeners to navigation elements

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+
+// Navigation Header 
+let navigationHeader = document.getElementsByClassName('logo-heading');
+navigationHeader[0].addEventListener('click', event => {
+    event.target.style.color = 'red';
+})
+
+// Navigation Links
+let navigationLinks = document.querySelectorAll('.nav-link')
+navigationLinks.forEach(element => {
+    element.addEventListener('mouseover', event => {
+        event.target.style.color = 'orange';
+    })
+})
+
+navigationLinks.forEach(element => {
+    element.addEventListener('mouseout', event => {
+        event.target.style.display = 'none'
+    })
+})
